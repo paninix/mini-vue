9683c473858f3fa802457d23999ca9a2f69d43a8
xuguobin <xuguobin@dw.gdbbk.com>
2024-10-02 11:15:31 +0800

    fix(reactivity): 完成 reactive及baseHandlers

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -15,17 +15,52 @@ const readonlyGet = createGetter(true);
 const shallowReadonlyGet = createGetter(true, true);
 
 function createGetter(isReadonly = false, shallow = false) {
+  return function getter(target, key, receiver) {
+    if(key === ReactiveFlags.IS_REACTIVE) {
+      return !isReadonly;
+    } else if(key === ReactiveFlags.IS_READONLY) {
+      return isReadonly;
+    } else if(
+      key === ReactiveFlags.RAW && (
+        receiver === reactiveMap.get(target) || 
+        receiver === readonlyMap.get(target) || 
+        receiver === shallowReadonlyMap.get(target)
+      ) ) {
+      return target;
+    }
+    const res = Reflect.get(target, key, receiver);
+
+    if(shallow) {
+      // 浅层处理
+      return res;
+    }
+
+    if(isObject(res)) {
+      return isReadonly? readonly(res) : reactive(res);
+    }
+
+    return res;
+  }
 }
 
 function createSetter() {
 }
 
 export const readonlyHandlers = {
- 
+ get: readonlyGet,
+ set: (key, value, target) =>{
+   throw new Error("Set operation on key " + key + " is not allowed");
+ }
 };
 
 export const mutableHandlers = {
+  get,
+  set
 };
 
 export const shallowReadonlyHandlers = {
+  get: shallowReadonlyGet,
+  set: (key, value, target) => {
+    throw new Error("Set operation on key " + key + " is not allowed");
+  }
 };
--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -15,25 +15,43 @@ export const enum ReactiveFlags {
 }
 
 export function reactive(target) {
+  return createReactiveObject(target, reactiveMap, mutableHandlers);
 }
 
 export function readonly(target) {
+  return createReactiveObject(target, readonlyMap, readonlyHandlers);
 }
 
 export function shallowReadonly(target) {
+  return createReactiveObject(target, shallowReadonlyMap, shallowReadonlyHandlers);
 }
 
 export function isProxy(value) {
+  // TODO: 这里和源码不一样
+  return isReactive(value) || isReadonly(value)
 }
 
 export function isReadonly(value) {
+  return !!( value && (value[ReactiveFlags.IS_READONLY]))
 }
 
 export function isReactive(value) {
+  return !!( value && (value[ReactiveFlags.IS_REACTIVE]))
 }
 
 export function toRaw(value) {
+  // TODO: 这里和源码不一样
+  // 对返回的value[ReactiveFlags.RAW] 再做一层toRaw 防止返回的还是Proxy
+  return  isProxy(value) ? toRaw(value[ReactiveFlags.RAW]) : value;
 }
 
 function createReactiveObject(target, proxyMap, baseHandlers) {
+  if(proxyMap.has(target)) {
+    // target already has a reactive proxy, return it.
+    return proxyMap.get(target);
+  }
+
+  const proxyObj =  new Proxy(target, baseHandlers);
+  proxyMap.set(target, proxyObj);
+  return proxyObj;
 }
